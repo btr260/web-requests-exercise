1cbfbd9bac7c3b7bcbdcde8af652cddd02c3625d
btr260 <65568934+btr260@users.noreply.github.com>
2020-06-09 20:16:32 -0400

    set up repo and first task

--- get_data.py
+++ get_data.py
@@ -1,3 +1,17 @@
 # get_data.py
 
+import json
+
+prod_path = "https://raw.githubusercontent.com/prof-rossetti/intro-to-python/master/data/products/2.json"
+
+with open(prod_path, "r") as json_file:
+    file_contents = json_file.read()
+
+products = json.loans(file_contents)
+
+print(type(products))
+print(products)
+
+
+
 print("REQUESTING SOME DATA FROM THE INTERNET...")
