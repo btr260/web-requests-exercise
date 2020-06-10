5965504d42451c13a616351a82894759e581874d
btr260 <65568934+btr260@users.noreply.github.com>
2020-06-09 21:02:40 -0400

    completed exercise

--- get_data.py
+++ get_data.py
@@ -35,3 +35,25 @@ print(type(prod2_data))
 
 for x in prod2_data:
     print(f"{x['name']} (Product ID: {x['id']}")
+
+
+
+#-----------------------------------------------------
+
+gb_path = "https://raw.githubusercontent.com/prof-rossetti/intro-to-python/master/data/gradebook.json"
+
+gb = requests.get(gb_path)
+gb_data = json.loads(gb.text)
+
+#print(gb_data)
+#print(gb_data.keys())
+
+grades = [x['finalGrade'] for x in gb_data['students']]
+#print(test)
+#print(type(test))
+
+min_grade = min(grades)
+max_grade = max(grades)
+avg_grade = sum(grades) / len(grades)
+
+print(f"Min Grade: {min_grade}, Max Grade: {max_grade}, and Average Grade: {avg_grade:.2f}")
